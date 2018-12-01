2fe8f1112e94ad96039b428ee0e773aa18ed3005
MarcoTS <mrctrgrst@gmail.com>
2018-12-01 00:03:25 -0600

    Iconos agregados

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
-//import WeatherTemparature from './WeatherTemperature';
+import WeatherTemparature from './WeatherTemperature';
+
+
 const WeatherData = () => (
 <div>
 <h2>Weather Data</h2>
+<WeatherTemparature temparature = {20} weatherState={''}/>
 <WeatherExtraInfo humidity={80} wind={"20km"}/>
 
 </div>
--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemparature = () => (
-<div>
-<h2><span>25°</span></h2>
-</div>
+
+const getStateIcon = {
+    sunny: "day-sunny",
+    light_wind: "day-light-wind",
+    rainy: "sleet",
+    fog: "day-fog"
+}
+
+const getWeatherIcon = weatherState => {
+    const icon = getStateIcon[weatherState];
+    if (icon)
+        return <WeatherIcons name={icon} size="2x" />;
+    else    
+        return <WeatherIcons name={"day-sunny"} size="2x" />;
+
+}
+
+const WeatherTemparature = ({temparature, weatherState}) =>
+(
+    <div>
+       { getWeatherIcon(weatherState) }
+        <h2><span>{`${temparature}`}</span></h2>
+    </div>
 );
 
 export default WeatherTemparature;
