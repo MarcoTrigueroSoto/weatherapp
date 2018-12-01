d4b80415d0a0e0e7e8b0f6ab29488837243d0956
MarcoTS <mrctrgrst@gmail.com>
2018-12-01 01:49:00 -0600

    Prototypes validations location

--- weather-app/src/components/Location.js
+++ weather-app/src/components/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = ({city}) => (
 //desestructuración
@@ -9,5 +10,9 @@ const Location = ({city}) => (
         </h1>
     </div>
 );
+
+Location.protTypes = {
+    city: PropTypes.string.isRequired,
+}
 
 export default Location;
